13d6fe0e64193e44bad8cf13cf5431ecb67f679f
BuildTools <unconfigured@null.spigotmc.org>
2024-11-02 11:49:43 -0600

    adds new Vector2 class to be used for points

---
+++ src/Vector2.java
@@ -0,0 +1,33 @@
+public class Vector2 {
+    double x;
+    double y;
+    public Vector2(double _x, double _y){
+        x=_x;
+        y=_y;
+    }
+    public double getDist(){
+        return Math.sqrt(x*x+y*y);
+    }
+    //math classes
+    public Vector2 add(Vector2 b){
+        return new Vector2(x+b.x,y+b.y);
+    }
+    public Vector2 subtract(Vector2 b){
+        return new Vector2(x-b.x,y-b.y);
+    }
+    public Vector2 multiply(Vector2 b){
+        return new Vector2(x*b.x,y*b.y);
+    }
+    public Vector2 divide(Vector2 b){
+        return new Vector2(x/b.x,y/b.y);
+    }
+    public Vector2 multiply(double b){
+        return new Vector2(x*b,y*b);
+    }
+    public Vector2 divide(double b){
+        return new Vector2(x/b,y/b);
+    }
+    public Vector2 normalize(){
+        return this.divide(this.getDist());
+    }
+}
